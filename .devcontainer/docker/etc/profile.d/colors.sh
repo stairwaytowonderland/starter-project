d0960caab7f385610647a976b0e5a53530d74e21
Andrew Haller <andrewhaller101@gmail.com>
2026-02-23 13:34:43 -0600

    feat: colors update

--- .devcontainer/docker/etc/profile.d/colors.sh
+++ .devcontainer/docker/etc/profile.d/colors.sh
@@ -3,11 +3,18 @@
 # Usage: bash /path/to/colors.sh [OPTIONS] [GRID_COLS]
 #
 #   OPTIONS:
-#     -256, --extended:     Show the full 256-color palette and corresponding escape codes (default: unset);
-#                           Omit for standard 8-bit ANSI color codes with brightness variants.
-#     -b, --bgchar [CHAR]:  Character to use for background color demonstration in the color (palette) grid (default: '█')
-#     -n, --same-line:      Print foreground-only colors on the same line (default: false)
+#     -256, --extended:     Show the full 256-color palette and corresponding
+#                           escape codes (default: unset).
+#                           Omit for standard 8-bit ANSI color codes with
+#                           brightness variants.
+#     -b, --bgchar [CHAR]:  Character to use for background color demonstration
+#                           in the color (palette) grid (default: '█')
+#     -g, --grid:           Show only the color grid (default: false).
 #     -h, --help:           Show this help message and exit
+#     -n, --same-line:      Print foreground-only colors on the same line
+#                           (default: false)
+#     -p, --palette:        Show only the color palette without grid
+#                           or escape codes (default: false)
 #
 #   GRID_COLS: Number of columns to display in the color grid (default: 8)
 #
@@ -17,6 +24,55 @@
 #   bash colors.sh -n
 #   bash colors.sh -256 -n -b '██' 16
 #   bash colors.sh 4
+#   bash colors.sh -p
+#   bash colors.sh -p -256
+#   bash colors.sh -p -8 -n
+
+palette256() {
+    color() {
+        local c
+        for c; do
+            printf '\e[%s;5;%dm%03d' "$bg" "$c" "$c"
+        done
+        printf '\e[0m \n'
+    }
+    local IFS=$' \t\n' bg i
+    for bg in 38 48; do
+        color {0..15}
+        for i in {0..5}; do
+            color $(seq $((i * 36 + 16)) $((i * 36 + 51)))
+        done
+        color {232..255}
+    done
+}
+
+palette8() {
+    color() {
+        for c; do
+            printf '\e[%dm%03d' "$c" "$c"
+        done
+        printf '\e[0m \n'
+    }
+    local sameline="${1:-false}" IFS=$' \t\n'
+    if $sameline; then
+        color {30..37} {90..97}
+        color {40..47} {100..107}
+    else
+        for base in 30 90; do
+            color $(seq $base $((base + 7)))
+        done
+        for base in 40 100; do
+            color $(seq $base $((base + 7)))
+        done
+    fi
+}
+
+palette() {
+    printf "Standard 8-color ANSI palette with brightness variants:\n"
+    palette8 true
+    printf "Extended 256-color palette:\n"
+    palette256
+}
 
 colorgrid256() {
     local cols="${1:-8}" bgchar="${2:-█}" i
@@ -150,18 +206,23 @@ Description: Display colors with corresponding ANSI or EXTENDED escape codes,
 Example: $script_name -256 -n -b '⬤ ' 16
 GRID_COLS is the number of columns to display in the color grid (default: 8).
 
-Palette:
+Display Options:
   -256, --extended:     Show the full 256-color palette and corresponding
-                        escape codes (default: unset); Omit for standard
-                        8-bit ANSI color codes with brightness variants.
+                        escape codes (default: unset);
+                        Omit for standard 8-bit ANSI color codes with
+                        brightness variants.
   -b, --bgchar [CHAR]:  Character(s) to use for background color demonstration
-                        in the color (palette) grid (default: '█')
+                        in the color (palette) grid (default: '█').
+  -g, --grid:           Show only the color grid (default: false).
+  -p, --palette:        Show only the color palette without grid or
+                        escape codes (default: false).
 
 Formatting:
-  -n, --same-line:      Print foreground-only colors on the same line (default: false)
+  -n, --same-line:      Print foreground-only colors on the same line
+                        (default: false).
 
 Miscellaneous:
-  -h, --help:           Show this help message and exit
+  -h, --help:           Show this help message and exit.
 
 Additional Examples:
 \$ $script_name
@@ -169,15 +230,24 @@ Additional Examples:
 \$ $script_name -n
 \$ $script_name -256 -n -b '██' 16
 \$ $script_name 4
+\$ $script_name -p
+\$ $script_name -p -256
+\$ $script_name -p -8 -n
 EOF
 
     [ -z "$exit" ] || return "$exit"
 }
 
 colors() {
-    local extended=false sameline=false _cols=8 cols bgchar _missing
+    local extended=false explicit8=false sameline=false grid=false palette=false
+    local cols bgchar _errmsg=() _err=0 _cols=8
     while [ "$#" -gt 0 ]; do
         case "$1" in
+            -8 | --standard)
+                extended=false
+                explicit8=true
+                shift
+                ;;
             -256 | --extended)
                 extended=true
                 shift
@@ -187,18 +257,26 @@ colors() {
                     bgchar="$2"
                     shift 2
                 else
-                    _missing="$1"
+                    _errmsg+=("Error: Missing argument for ${1}")
                     shift
                     break
                 fi
+                ;;
+            -h | --help)
+                colorsusage
+                return 0
+                ;;
+            -g | --grid)
+                grid=true
+                shift
                 ;;
             -n | --same-line)
                 sameline=true
                 shift
                 ;;
-            -h | --help)
-                colorsusage
-                return 0
+            -p | --palette)
+                palette=true
+                shift
                 ;;
             *[!0-9]*)
                 shift
@@ -208,18 +286,33 @@ colors() {
                 ;;
         esac
     done
-    if [ -n "${_missing-}" ]; then
-        echo "Error: Missing argument for ${_missing}" >&2
-        colorsusage 1
-        # local _err=$?
-        # [ "$_err" -eq 0 ] || return "$_err"
+    if [ "${#_errmsg[@]}" -gt 0 ]; then
+        printf '%s\n' "${_errmsg[@]}" >&2
+        colorsusage "${#_errmsg[@]}"
+    fi
+    _err=$?
+    [ "$_err" -eq 0 ] || return "$_err"
+    if [[ $1 =~ ^[0-9]+$ ]]; then
+        cols="$1"
+        shift
     else
-        if [[ $1 =~ ^[0-9]+$ ]]; then
-            cols="$1"
-            shift
+        cols="$_cols"
+    fi
+    if $palette; then
+        if $explicit8; then
+            palette8 $sameline
+        elif $extended; then
+            palette256
         else
-            cols="$_cols"
+            palette
         fi
+    elif $grid; then
+        if $extended; then
+            colorgrid256 "$cols" "$bgchar"
+        else
+            colorgrid8_with_brightness "$cols" "$bgchar"
+        fi
+    else
         if $extended; then
             colors256 $sameline
             ! $sameline || echo
